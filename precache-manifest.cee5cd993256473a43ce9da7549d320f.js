c213a729dbcd02a66cf3a23d2cdc47e9d4339da9
Jürgen <behighspy@yahoo.de>
2018-11-15 16:09:26 +0100

    Updates

---
+++ precache-manifest.cee5cd993256473a43ce9da7549d320f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6dfabb4756bcf8062d6a9028095e9a6",
+    "url": "/static/media/portrait_01-small.e6dfabb4.png"
+  },
+  {
+    "revision": "e04b0c2c0156ea5beb228840ce115086",
+    "url": "/static/media/not_found.e04b0c2c.png"
+  },
+  {
+    "revision": "24350adbebaa6a0b2324892bf51747a5",
+    "url": "/static/media/fernglas_02_win_01-01.24350adb.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9013e9d3e6d1c24b47a8",
+    "url": "/static/js/main.9013e9d3.chunk.js"
+  },
+  {
+    "revision": "aabc85f7d99c4ef5d9e2",
+    "url": "/static/js/1.aabc85f7.chunk.js"
+  },
+  {
+    "revision": "9013e9d3e6d1c24b47a8",
+    "url": "/static/css/main.c8fe510a.chunk.css"
+  },
+  {
+    "revision": "b3d07439ba474138951e0fd6958be55b",
+    "url": "/index.html"
+  }
+];
